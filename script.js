bf0884af37527b3e5dc4fe3ac886448292908e28
Fichi 3 <70333084+Fichiiiii@users.noreply.github.com>
2024-02-23 14:54:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ function shiftnavbar() {
     const navTransform = document.getElementById("shiftnav").style.transform
     const isExpanded = navTransform == "translateX(0px)"
 
+    document.getElementById("owg-text").style.marginTop = "-8px"
+
     document.getElementById("shiftnav").style.transform = `translateX(${isExpanded * -290}px)`
     document.getElementById("content").style.transform = `translateX(${isExpanded ? 0 : 1 * 290}px)`
 
