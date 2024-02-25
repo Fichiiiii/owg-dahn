24cf106353999cbee9c5981890771dc20caec872
Fichi 3 <70333084+Fichiiiii@users.noreply.github.com>
2024-02-25 21:05:17 +0100

    Update addContactEntry.js

--- api/addContactEntry.js
+++ api/addContactEntry.js
@@ -2,16 +2,22 @@ exports.handler = async (event, context) => {
   if (event.httpMethod === 'POST') {
     try {
       const requestData = event.body
-      //const { input } = requestData
-      
+
+      if (!requestData.startsWith("input=")) return {
+        statusCode: 400,
+        body: JSON.stringify({ error: "Invalid Request Body" })
+      }
+
+      const message = requestData.replace("input=", "")
+
       return {
         statusCode: 200,
-        body: JSON.stringify({ message: requestData }),
+        body: JSON.stringify({ message: "Request processed successfully", message: message })
       }
     } catch (error) {
       return {
         statusCode: 400,
-        body: JSON.stringify({ error: 'Failed to process POST request' }),
+        body: JSON.stringify({ error: "Failed to process POST request" })
       }
     }
   }
