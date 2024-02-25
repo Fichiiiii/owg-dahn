22f51f7703908f5a1385d9a111e50feff924d206
Fichi 3 <70333084+Fichiiiii@users.noreply.github.com>
2024-02-26 00:03:55 +0100

    Update addContactEntry.js

--- api/addContactEntry.js
+++ api/addContactEntry.js
@@ -1,6 +1,61 @@
+const redis = require('redis')
+
+const client = createClient({
+  password: 'process.env.REDIS_PASS',
+  socket: {
+      host: 'redis-17814.c300.eu-central-1-1.ec2.cloud.redislabs.com',
+      port: 17814
+  }
+})
+
+async function dataExists(key) {
+  return new Promise((resolve, reject) => {
+    client.exists(key, (err, reply) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(reply === 1)
+      }
+    })
+  })
+}
+
+async function getData(key) {
+  return new Promise((resolve, reject) => {
+    client.get(key, (err, reply) => {
+      if (err) {
+          reject(err)
+        } else {
+          resolve(reply)
+        }
+    })
+  })
+}
+
+async function setEntry(key, value) {
+  return new Promise((resolve, reject) => {
+    client.set(key, value, (err, reply) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(reply)
+      }
+    })
+  })
+}
+
 exports.handler = async (event, context) => {
   if (event.httpMethod === 'POST') {
     try {
+      // Connect to a Redis Database
+      client.on('error', (err) => {
+        return {
+          statusCode: 500,
+          body: JSON.stringify({ message: "Internal Server Error" })
+        }
+      })
+
+      // Process the request
       const requestData = event.body
 
       if (!requestData.startsWith("input=")) return {
@@ -10,6 +65,11 @@ exports.handler = async (event, context) => {
 
       const message = requestData.replace("input=", "")
       const input = decodeURIComponent(message.replaceAll('+', ' '))
+
+      let contactEntries = dataExists("contactEntries") ? JSON.parse(getData("contactEntries")) : []
+      contactEntries.push(input)
+
+      await setEntry("contactEntries", contactEntries)
 
       return {
         statusCode: 200,
