82107c59e017021dcb458438d5753ffe7b5202f7
Fichi 3 <70333084+Fichiiiii@users.noreply.github.com>
2024-02-25 16:49:47 +0100

    Update addContactEntry.js

--- api/addContactEntry.js
+++ api/addContactEntry.js
@@ -1,12 +1,8 @@
 exports.handler = async (event, context) => {
   if (event.httpMethod === 'POST') {
     try {
+      console.log(event.httpMethod)
       console.log(event)
-      
-      const requestBody = JSON.parse(event.body)
-
-      console.log(requestBody)
-
       return {
         statusCode: 200,
         body: JSON.stringify({ message: 'POST request processed successfully' }),
