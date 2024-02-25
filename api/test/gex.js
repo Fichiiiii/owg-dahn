aa00e892b37b340d24f62c867b63dd7af44c6d73
Fichi 3 <70333084+Fichiiiii@users.noreply.github.com>
2024-02-25 14:18:03 +0100

    Create gex.js

---
+++ api/test/gex.js
@@ -0,0 +1,19 @@
+// api.js
+
+exports.handler = async (event, context) => {
+    if (event.httpMethod === 'GET') {
+      try {
+        // Return the data as the response
+        return {
+          statusCode: 200,
+          body: JSON.stringify({message: "test"}),
+        };
+      } catch (error) {
+        // Return an error response if there was an issue processing the request
+        return {
+          statusCode: 500,
+          body: JSON.stringify({ error: 'Failed to process GET request' }),
+        };
+      }
+    }
+  };
