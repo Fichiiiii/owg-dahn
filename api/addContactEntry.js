778c4c73ddcd5987f12c6bed5996a1d60dc5803e
Fichi 3 <70333084+Fichiiiii@users.noreply.github.com>
2024-02-29 00:05:28 +0100

    Update addContactEntry.js

--- api/addContactEntry.js
+++ api/addContactEntry.js
@@ -1,49 +1,11 @@
-/*const redis = require('redis')
+const { MongoClient } = require("mongodb");
 
-const client = createClient({
-  password: 'process.env.REDIS_PASS',
-  socket: {
-      host: 'redis-17814.c300.eu-central-1-1.ec2.cloud.redislabs.com',
-      port: 17814
-  }
-})
+const mongoClient = new MongoClient(process.env.MONGODB_URI);
 
-async function getData(key) {
-  return new Promise((resolve, reject) => {
-    client.get(key, (err, reply) => {
-      if (err) {
-          reject(err)
-        } else {
-          resolve(reply)
-        }
-    })
-  })
-}
+mongoClient.connect();
 
-async function setEntry(key, value) {
-  return new Promise((resolve, reject) => {
-    client.set(key, value, (err, reply) => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(reply)
-      }
-    })
-  })
-}
-
-exports.handler = async (event, context) => {
-  if (event.httpMethod === 'POST') {
+const handler = async (event) => {
     try {
-      // Connect to a Redis Database
-      client.on('error', (err) => {
-        return {
-          statusCode: 500,
-          body: JSON.stringify({ message: "Internal Server Error" })
-        }
-      })
-
-      // Process the request
       const requestData = event.body
 
       if (!requestData.startsWith("input=")) return {
@@ -53,21 +15,16 @@ exports.handler = async (event, context) => {
 
       const message = requestData.replace("input=", "")
       const input = decodeURIComponent(message.replaceAll('+', ' '))
+    
+      await client.db("contact").collection("messages").insertOne({ message: input })
 
-      let contactEntries = JSON.parse(getData("contactEntries")) ?? []
-      contactEntries.push(input)
-
-      await setEntry("contactEntries", contactEntries)
-
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Request processed successfully", input: input })
-      }
+        return {
+            statusCode: 200,
+            body: JSON.stringify(results),
+        }
     } catch (error) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ message: "Failed to process POST request" })
-      }
+        return { statusCode: 500, body: error.toString() }
     }
-  }
-}*/
+}
+
+module.exports = { handler }
