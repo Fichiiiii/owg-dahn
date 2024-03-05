cf80dd8f3abcb6135469fa1722af0e384302853f
Fichi 3 <70333084+Fichiiiii@users.noreply.github.com>
2024-03-05 22:13:39 +0100

    Update addContactEntry.js

--- api/addContactEntry.js
+++ api/addContactEntry.js
@@ -5,24 +5,32 @@ const mongoClient = new MongoClient(process.env.MONGODB_URI)
 mongoClient.connect()
 
 const handler = async (event) => {
-    try {
-      const requestData = event.body
+  try {
+    const requestData = event.body
 
-      if (requestData.length < 25) return {
-        statusCode: 400,
-        body: JSON.stringify({ message: "Invalid Request Body" })
-      }
-
-      const input = decodeURIComponent(requestData.replaceAll('+', ' '))
-      await mongoClient.db("contact").collection("messages").insertOne({ message: input })
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ message: "Request processed successfully", input: input }),
-        }
-    } catch (error) {
-        return { statusCode: 500, body: error.toString() }
+    if (event.httpMethod != 'POST') return {
+      statusCode: 405,
+      body: JSON.stringify({ message: "Method Not Allowed" })
     }
+
+    if (requestData.length < 25) return {
+      statusCode: 400,
+      body: JSON.stringify({ message: "Invalid Request Body" })
+    }
+
+    const input = decodeURIComponent(requestData.replaceAll('+', ' '))
+    await mongoClient.db("contact").collection("messages").insertOne({ message: input })
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Request processed successfully", input: input }),
+    }
+  } catch (error) {
+    return { 
+      statusCode: 500, 
+      body: error.toString() 
+    }
+  }
 }
 
 module.exports = { handler }
