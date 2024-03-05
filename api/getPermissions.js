5f50ba5c2f308fe4cee1712191d62e270046eaab
Fichi 3 <70333084+Fichiiiii@users.noreply.github.com>
2024-03-05 23:02:16 +0100

    Update getPermissions.js

--- api/getPermissions.js
+++ api/getPermissions.js
@@ -1,12 +1,24 @@
+const { MongoClient } = require("mongodb");
+
+const mongoClient = new MongoClient(process.env.MONGODB_URI)
+
+mongoClient.connect()
+
 exports.handler = async (event) => {
     try {
+        if (event.httpMethod != 'GET') return {
+            statusCode: 405,
+            body: JSON.stringify({ message: "Method Not Allowed" })
+        }
+
         const queryParams = event.queryStringParameters
-    
         const key = queryParams.key ?? ""
     
+        const user = await mongoClient.db("accounts").collection("accountData").findOne({ key: key}, {})
+
         return {
             statusCode: 200,
-            body: JSON.stringify({ key })
+            body: JSON.stringify({ name: user.name, perms: user.permitions })
         }
     } catch (error) {
         return { 
