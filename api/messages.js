5875b7208b7cec9b29704b546079ded1d1939a45
Isabelle von Borstel <noah.vonborstel@gmx.de>
2024-05-07 21:38:39 +0200

    Added messages api endpoint to view messages sent through the contact form

---
+++ api/messages.js
@@ -0,0 +1,29 @@
+const { MongoClient } = require("mongodb");
+
+const mongoClient = new MongoClient(process.env.MONGODB_URI)
+
+mongoClient.connect()
+
+const handler = async (event) => {
+  try {
+    if (event.httpMethod != 'GET') return {
+      statusCode: 405,
+      body: JSON.stringify({ message: "Method Not Allowed" })
+    }
+
+    const queryParams = event.queryStringParameters
+    const key = queryParams.key ?? ""
+
+    return { 
+        statusCode: 200, 
+        body: key.toString()
+      }
+  } catch (error) {
+    return { 
+      statusCode: 500, 
+      body: error.toString() 
+    }
+  }
+}
+
+module.exports = { handler }
